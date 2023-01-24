1c9dcf67efc3f9408f4425ddc6a8aad079a9f70d
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-01-24 09:14:37 -0500

    update multiplecustomhook

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FormWithCustomHook } from './02-useEfect/FormWithCustomHook';
 import { Message } from './02-useEfect/Message';
+import { MultipleCustomhooks } from './03-examples/MultipleCustomhooks';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './02-useEfect/simpleForm';
@@ -11,6 +12,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FormWithCustomHook/>
+    <MultipleCustomhooks/>
   </React.StrictMode>
 )
